dcdd68990bb2e353c7e7b8b5f9d327155c883fc0
Heyder Gustavo <heydergalo@gmail.com>
2021-06-05 19:47:09 -0300

    3 aula

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do 
+  resources :restaurants
+  resources :products, only: [:index, :new, :create]
   devise_for :users
   root 'home#index'  
 end
